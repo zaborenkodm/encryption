6677990423659edb2fa301b592223d2e60f9cb69
zaborenkodm <zaborenkodm@gmail.com>
2016-10-28 17:07:10 +0300

    программа шифрует введенное слово методами MD5 и SHA1

---
+++ encryption.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+# Программа, которая шифрует введённое пользователем слово
+# одним из механизмов MD5 или SHA1
+
+require 'digest'
+
+if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+puts "Введите слово или фразу для шифрования:"
+phrase = STDIN.gets.encode('UTF-8').chomp
+puts
+puts "Каким способом зашифровать:"
+puts "1. MD5"
+puts "2. SHA1"
+encryption_id = STDIN.gets.to_i
+puts
+puts "Вот что получилось:"
+
+if encryption_id == 1
+  puts "#{Digest::MD5.hexdigest(phrase)}"
+elsif encryption_id == 2
+  puts "#{Digest::SHA1.hexdigest(phrase)}"
+else
+  puts "Вы ввели недопустимое значение"
+end
